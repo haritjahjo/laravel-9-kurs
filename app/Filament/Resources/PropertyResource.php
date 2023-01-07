acb4401cd0b67d022ba1426c5065a0a0d2db5311
Hari Tjahjo <haritjahjo@gmail.com>
2023-01-07 23:06:46 +0700

    naming of images

--- app/Filament/Resources/PropertyResource.php
+++ app/Filament/Resources/PropertyResource.php
@@ -8,23 +8,26 @@ use App\Models\Property;
 use Filament\Resources\Form;
 use Filament\Resources\Table;
 use Filament\Resources\Resource;
+use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\Toggle;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\RichEditor;
 use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\PropertyResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use App\Filament\Resources\PropertyResource\RelationManagers;
-use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\Tabs;
 
 class PropertyResource extends Resource
 {
     protected static ?string $model = Property::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $modelLabel = 'property';
+    protected static ?string $pluralModelLabel = 'properties';
 
     public static function form(Form $form): Form
     {
@@ -32,45 +35,69 @@ class PropertyResource extends Resource
             Tabs::make('Heading')
                 ->tabs([
                     Tabs\Tab::make('Main Data')
+                        ->columns(columns:12)
                         ->schema([
                             TextInput::make('title')
+                                ->columnSpan(span:12)
                                 ->required()
                                 ->maxLength(255),
                             RichEditor::make('description')
+                                ->columnSpan(span:12)
                                 ->required()
                                 ->maxLength(65535),
                             TextInput::make('country')
+                                ->columnSpan(span:6)
                                 ->required()
                                 ->maxLength(255),
                             TextInput::make('city')
+                                ->columnSpan(span:6)
                                 ->required()
                                 ->maxLength(255),
                             TextInput::make('address')
+                                ->columnSpan(span:6)
                                 ->required()
                                 ->maxLength(255),
                             TextInput::make('price')
+                                ->columnSpan(span:6)
                                 ->required(),
                             TextInput::make('sqm')
+                                ->columnSpan(span:3)
                                 ->required(),
                             TextInput::make('bedrooms')
+                                ->columnSpan(span:3)
                                 ->required(),
                             TextInput::make('bathrooms')
+                                ->columnSpan(span:3)
                                 ->required(),
                             TextInput::make('garages')
+                                ->columnSpan(span:3)
                                 ->required(),
                             Toggle::make('slider')
+                                ->columnSpan(span:12)
                                 ->required(),
 
                         ]),
                     Tabs\Tab::make('Period')
+                        ->columns(columns:12)
                         ->schema([
-                            DatePicker::make(name:'start_date'),
-                            DatePicker::make(name:'end_date'),
+                            DatePicker::make(name:'start_date')
+                                ->columnSpan(span:6),
+                            DatePicker::make(name:'end_date')
+                                ->columnSpan(span:6),
                         ]),
                     Tabs\Tab::make('Picture')
                         ->schema([
-                            // ...
-                        ]),
+                            SpatieMediaLibraryFileUpload::make(name:'Slider Image')
+                                ->image()
+                                ->collection(collection:'slider')
+                                ->columnSpan(span: 6),
+                            SpatieMediaLibraryFileUpload::make(name:'Thumbnail Slider (Bitte speichren !)')
+                                ->image()
+                                ->multiple()
+                                ->enableReordering()
+                                ->collection(collection:'thumb-slider')
+                                ->columnSpan(span: 6),
+                        ])->columns(columns: 12),
                 ])
 
             
