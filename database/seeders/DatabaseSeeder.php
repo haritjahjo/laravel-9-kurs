3d7ce9c7eec8955777b4d370424171e20421f8c5
Hari Tjahjo <haritjahjo@gmail.com>
2023-01-07 11:16:18 +0700

    Add user seeder and set faker locale

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Hari Tjahjo',
+            'email' => 'hari@example.com',
+            'password' => bcrypt(value:'password'),
+        ]);
     }
 }
