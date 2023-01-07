e68c22e3ab50156e6981afa27aa95deca98c4442
Hari Tjahjo <haritjahjo@gmail.com>
2023-01-07 12:40:18 +0700

    Resource Property --generate --soft-deletes --view

---
+++ app/Filament/Resources/PropertyResource.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PropertyResource\Pages;
+use App\Filament\Resources\PropertyResource\RelationManagers;
+use App\Models\Property;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PropertyResource extends Resource
+{
+    protected static ?string $model = Property::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->maxLength(65535),
+                Forms\Components\Toggle::make('slider')
+                    ->required(),
+                Forms\Components\TextInput::make('country')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('city')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('address')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('price')
+                    ->required(),
+                Forms\Components\TextInput::make('sqm')
+                    ->required(),
+                Forms\Components\Toggle::make('bedrooms')
+                    ->required(),
+                Forms\Components\Toggle::make('bathrooms')
+                    ->required(),
+                Forms\Components\Toggle::make('garages')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\IconColumn::make('slider')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('country'),
+                Tables\Columns\TextColumn::make('city'),
+                Tables\Columns\TextColumn::make('address'),
+                Tables\Columns\TextColumn::make('price'),
+                Tables\Columns\TextColumn::make('sqm'),
+                Tables\Columns\IconColumn::make('bedrooms')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('bathrooms')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('garages')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\ForceDeleteBulkAction::make(),
+                Tables\Actions\RestoreBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProperties::route('/'),
+            'create' => Pages\CreateProperty::route('/create'),
+            'view' => Pages\ViewProperty::route('/{record}'),
+            'edit' => Pages\EditProperty::route('/{record}/edit'),
+        ];
+    }    
+    
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
